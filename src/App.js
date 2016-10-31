8edf9b30afe178e674c17a62a8cb249f967b59c9
Krishcdbry <mohana.krishna@misport.com>
2016-10-31 12:17:52 +0530

    Guess App first commit

---
+++ src/App.js
@@ -0,0 +1,123 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class GuessKitty extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            NUMBER_OF_BLOCKS: 25
+            , blocks: []
+            , KITTY_INDEX: Math.floor((Math.random() * 10) + 1)
+            , win : false
+            , attempts: 0
+            , currentScore: 0
+            , scores : []
+        };
+
+        for (var i = 0; i < this.state.NUMBER_OF_BLOCKS; i++) {
+            var content = (i === this.state.KITTY_INDEX) ? 'KITTY' : 'X';
+            this.state.blocks.push(content);
+        }
+    }
+
+    getScoreUpdate() {
+        var length = this.state.scores.length;
+        var scores = this.state.scores;
+        if (length > 0) {
+            this.totalScore = scores.reduce(function(next, prev) {
+                return next + prev;
+            });
+
+            this.avgScore = Math.round(this.totalScore / length);
+            this.minScore = Math.round(scores.sort()[0]);
+            this.maxScore = Math.round(scores.sort()[length - 1]);
+        }
+    }
+
+    handleClick(index) {
+        var blocks =  this.state.blocks;
+        var scores = this.state.scores;
+        if (this.state.blocks[index] !== 'VISITED') {
+            blocks[index] = 'VISITED';
+            if (index === this.state.KITTY_INDEX) {
+                var currentScore = (25-this.state.attempts)*100;
+                scores.push(currentScore);
+            }
+            this.state.attempts++;
+            this.setState({
+                blocks: blocks
+                , attempts: this.state.attempts
+                , win: (index === this.state.KITTY_INDEX)
+                , currentScore : currentScore
+                , scores: scores
+            });
+
+            this.getScoreUpdate();
+        }
+
+    }
+
+    handleClickRestart() {
+        var state = this.state;
+        state.NUMBER_OF_BLOCKS =  25;
+        state.blocks = [];
+        state.KITTY_INDEX = Math.floor((Math.random() * 10) + 1);
+        state.win = false;
+        state.attempts = 0;
+        state.currentScore = 0;
+
+        for (var i = 0; i < state.NUMBER_OF_BLOCKS; i++) {
+            var content = (i === state.KITTY_INDEX) ? 'KITTY' : 'X';
+            state.blocks.push(content);
+        }
+
+        this.setState(state);
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <h1>Guess Kitty - ReactJs</h1>
+                </div>
+                <center>
+                    <div className="game-area">
+                        <div className={"board " + (this.state.win ? "board won": "")}>
+                            {this.state.blocks.map((block, index) => {
+                                return  (block === 'VISITED')
+                                    ? (index === this.state.KITTY_INDEX)
+                                    ? <div onClick={() => this.handleClick(index)} className="block success"><i className="fa fa-smile-o"></i></div>
+                                    : <div onClick={() => this.handleClick(index)} className="block failed"><i className="fa fa-frown-o"></i></div>
+                                    : <div onClick={() => this.handleClick(index)} className="block"><span className="fa fa-lock"></span></div>;
+                            })}
+                        </div>
+                        <div className={"score " + (this.state.win ? "Winner-anim": "")}>
+                            <p>Kitty Found</p>
+                            <h3> SCORE = {this.state.currentScore} </h3>
+                            <br></br>
+                            <a href="javascript:;" onClick={() => this.handleClickRestart()} className="noDecoration">RESTART</a>
+                        </div>
+                    </div>
+                    <h3> ATTEMPTS = {this.state.attempts}/25 </h3>
+                    <div className={this.state.scores.length > 0 ? "scores": "scores hide-me"}>
+                        <h3>Scores</h3>
+                        <hr></hr>
+                        <div className="score-list">
+                            {this.state.scores.map((score, index) => {
+                                return <p><b>{index+1}.</b> {score}</p>
+                            })}
+                        </div>
+                        <b>Total</b> : {this.totalScore} <br></br>
+                        <b>Avg</b> : {this.avgScore} <br></br>
+                        <b>Min</b>  : {this.minScore} <br></br>
+                        <b>Max</b> : {this.maxScore} <br></br>
+                    </div>
+                </center>
+            </div>
+        );
+    }
+}
+
+export default GuessKitty;
